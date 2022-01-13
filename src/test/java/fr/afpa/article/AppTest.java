f343fbb4726c00e71270e7660241a5476cf55d27
fafa-a <fabienpuech1@gmail.com>
2022-01-13 17:28:54 +0100

    chore : initial commit

---
+++ src/test/java/fr/afpa/article/AppTest.java
@@ -0,0 +1,20 @@
+package fr.afpa.article;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
